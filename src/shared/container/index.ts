bb709c3b331d7593f821ec280dcaccdf710a0b9c
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-02-04 23:16:06 -0300

    users

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,11 @@
 import {container} from "tsyringe";
+import { IUsersRepository} from "../../modules/accounts/repositories/IUsersRepository";
 import {ICategoryRepository} from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository} from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+
 // ICategoriesRepository
 container.registerSingleton<ICategoryRepository>(
   "CategoriesRepository", 
@@ -12,4 +15,9 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository", 
   SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository", 
+  UsersRepository
 )
