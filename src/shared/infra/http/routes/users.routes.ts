3f29753529e9d0eda6ed4ca190ad97ac1ccbc47a
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2022-03-05 15:11:10 -0300

    sem cred

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from "../../../../config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 
 const usersRoutes = Router();
 
@@ -12,10 +13,12 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarUseCase = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
 
 usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarUseCase.handle);
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 
 export { usersRoutes};
